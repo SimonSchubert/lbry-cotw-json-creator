a607948e6796d7140cf7cd3fa489bd23e88aee53
Simon Schubert <sschubert89@gmail.com>
2020-12-18 10:40:44 +0100

    Add channel name parameter

--- create_cotw_json.py
+++ create_cotw_json.py
@@ -33,9 +33,10 @@ for line in channelsFile.readlines():
 
     for item in response["result"]["items"]:
         title = item["value"].get("title", "")
+        name = item.get("name", "")[1:]
         thumbnailUrl = item["value"]["thumbnail"]["url"]
         tags = item["value"].get("tags", [])
-        channel = {'week': week, 'claimId':claimId, 'title':title, 'thumbnailUrl':thumbnailUrl, 'tags':tags}
+        channel = {'week': week, 'name':name, 'claimId':claimId, 'title':title, 'thumbnailUrl':thumbnailUrl, 'tags':tags}
         channels.append(channel)
 
     week = week+1

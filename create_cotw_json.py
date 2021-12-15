086bb52a7991d0839dec9c843c5a8006c5ca108f
SimonSchubert <simon@librem.one>
2021-12-16 00:17:39 +0100

    Reformat file writing

--- create_cotw_json.py
+++ create_cotw_json.py
@@ -13,75 +13,59 @@ print("#####################")
 print()
 
 
-def get_info_by_claim_id(claim_id):
+def get_info_by_claim_id(claim_id, week):
     try:
         response = requests.post("http://localhost:5279",
                                  json={"method": "claim_search", "params": {"claim_id": claim_id.strip()}}).json()
+
+        for item in response["result"]["items"]:
+                name = item.get("name", "")[1:]
+                title = item["value"].get("title", "")
+                if title == "":
+                    title = name
+                thumbnail_url = item["value"]["thumbnail"]["url"]
+                tags = item["value"].get("tags", [])
+                return {'week': week, 'name': name, 'claimId': claimId, 'title': title, 'thumbnailUrl': thumbnail_url,
+                        'tags': tags}
     except:
         print("Error: connection to http://localhost:5279")
         print("Is LBRY running? https://lbry.tech/api/sdk")
         exit()
 
-    for item in response["result"]["items"]:
-        name = item.get("name", "")[1:]
-        title = item["value"].get("title", "")
-        if title == "":
-            title = name
-        thumbnail_url = item["value"]["thumbnail"]["url"]
-        tags = item["value"].get("tags", [])
-        return {'week': week, 'name': name, 'claimId': claimId, 'title': title, 'thumbnailUrl': thumbnail_url,
-                'tags': tags}
+
+with open('channels.txt') as channels_file:
+    channels = []
+
+    for week, line in enumerate(channels_file.readlines()):
+        claimId = line.split(",")[0]
+        print("Fetch info for COTW " + str(week) + ": " + claimId)
+        info = get_info_by_claim_id(claimId, (week+1))
+        if info is not None:
+            channels.append(info)
+
+    print("Export all.json")
+    with open('all.json', 'w') as f:
+        f.write(json.dumps(channels))
+
+    print("Export current.json")
+    with open('current.json', 'w') as f:
+        f.write(json.dumps(channels[-1]))
 
 
-try:
-    channels_file = open("channels.txt", "r")
-    nominations_file = open("nominations.txt", "r")
-except:
-    print("Error: channels.txt not found")
-    print("Copy channels.txt into same directory as the python script and run the script again.")
-    exit()
+with open('nominations.txt') as nominations_file:
+    channels = []
 
-channels = []
+    for week, line in enumerate(nominations_file.readlines()):
+        claimId = line.split(",")[0]
+        print("Fetch info for nomination: " + claimId)
+        info = get_info_by_claim_id(claimId, (week+1))
+        if info is not None:
+            channels.append(info)
 
-week = 1
-for line in channels_file.readlines():
-    claimId = line.split(",")[0]
-    print("Fetch info for COTW " + str(week) + ": " + claimId)
-    info = get_info_by_claim_id(claimId)
-    if info is not None:
-        channels.append(info)
-    week = week + 1
+    print("Export nominations.json")
+    with open('nominations.json', 'w') as f:
+        f.write(json.dumps(channels))
 
-channels_file.close()
-
-print("Export all.json")
-
-f = open("all.json", "w")
-f.write(json.dumps(channels))
-f.close()
-
-print("Export current.json")
-
-f = open("current.json", "w")
-f.write(json.dumps(channels[-1]))
-f.close()
-
-channels = []
-
-for line in nominations_file.readlines():
-    claimId = line.split(",")[0]
-    print("Fetch info for nomination: " + claimId)
-    info = get_info_by_claim_id(claimId)
-    if info is not None:
-        channels.append(info)
-
-nominations_file.close()
-
-print("Export nominations.json")
-
-f = open("nominations.json", "w")
-f.write(json.dumps(channels))
-f.close()
 
 print()
 print("Job done :)")

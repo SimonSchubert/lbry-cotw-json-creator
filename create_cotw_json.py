83edd04e229e4c806ef262d56f3fec5313782277
SimonSchubert <simon@librem.one>
2021-12-21 11:24:46 +0100

    Add COTW 67

--- create_cotw_json.py
+++ create_cotw_json.py
@@ -38,7 +38,7 @@ with open('channels.txt') as channels_file:
 
     for week, line in enumerate(channels_file.readlines()):
         claimId = line.split(",")[0]
-        print("Fetch info for COTW " + str(week) + ": " + claimId)
+        print("Fetch info for COTW " + str(week+1) + ": " + claimId)
         info = get_info_by_claim_id(claimId, (week+1))
         if info is not None:
             channels.append(info)
